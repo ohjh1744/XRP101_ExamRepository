e621b4ca02b5bb97f56b747e49beda14c0180568
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-11-19 13:50:12 +0900

    [FIX] DelayRoutine에서 Exit 제거, 상태여부 bool변수 추가. OnUpdate, Exit, Attack함수 수정

--- Q_04/Assets/Scripts/StateAttack.cs
+++ Q_04/Assets/Scripts/StateAttack.cs
@@ -8,6 +8,9 @@ public class StateAttack : PlayerState
 {
     private float _delay = 2;
     private WaitForSeconds _wait;
+
+    // 공격여부 불변수 추가
+    private bool _isFinishAttack;
     
     public StateAttack(PlayerController controller) : base(controller)
     {
@@ -20,19 +23,27 @@ public class StateAttack : PlayerState
         ThisType = StateType.Attack;
     }
 
+    // 공격상태이므로 bool 변수 true
     public override void Enter()
     {
+        _isFinishAttack = true;
         Controller.StartCoroutine(DelayRoutine(Attack));
     }
 
     public override void OnUpdate()
     {
         Debug.Log("Attack On Update");
+
+        // 공격이 끝나면 Idle상태로 전환.
+        if(_isFinishAttack == false)
+        {
+            Machine.ChangeState(StateType.Idle);
+        }
     }
 
     public override void Exit()
     {
-        Machine.ChangeState(StateType.Idle);
+        //Machine.ChangeState(StateType.Idle);
     }
 
     private void Attack()
@@ -43,11 +54,19 @@ public class StateAttack : PlayerState
             );
 
         IDamagable damagable;
+
+        //IDamagable을 보유한 몬스터만 TakeHit하도록 수정.
         foreach (Collider col in cols)
         {
             damagable = col.GetComponent<IDamagable>();
-            damagable.TakeHit(Controller.AttackValue);
+            if(damagable != null)
+            {
+                damagable.TakeHit(Controller.AttackValue);
+            }
         }
+
+        //공격이 끝나면 false
+        _isFinishAttack = false;
     }
 
     public IEnumerator DelayRoutine(Action action)
@@ -55,7 +74,8 @@ public class StateAttack : PlayerState
         yield return _wait;
 
         Attack();
-        Exit();
+        //Exit();
+
     }
 
 }
